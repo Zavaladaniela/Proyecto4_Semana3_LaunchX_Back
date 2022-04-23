9f92e4efc67d27e81e020f1fbd8359f29b53303b
Zavaladaniela <daniela.zavala107@gmail.com>
2022-04-22 23:12:14 -0500

    Probando el test, falla porque no existe clase

---
+++ test/services/userService.test.js
@@ -0,0 +1,18 @@
+const userService = require ('./../../app/services/userService')
+
+//REQ 1: Crea un nuevo usuario con lo definido en el sprint anterior
+// agregando una clase llamada USERSERVICE
+// debera tener un metodo create, que se puede usar sin instanciar (static method)
+// recibe solo el id, username y name
+describe ("Tests for userServices", () =>{
+    test ("1. Create a new user using the userService", () =>{
+        const user = userService.create (1,"danielazavala","dany")
+        expect (user.username).toBe("danielazavala")
+        expect (user.name).toBe("dany")
+        expect (user.id).toBe("1")
+        expect (user.bio).not.toBeUndefined()
+
+    })
+})
+
+// REQ 2: Agregar metodo estatico en userService llamado getInfo
